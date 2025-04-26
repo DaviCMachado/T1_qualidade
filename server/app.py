ff362d6f642799fd9a4828d702229b026ec3fd1d
Diego Ribeiro <drchaves@inf.ufsm.br>
2025-04-26 17:15:04 -0300

    adicionados os testes de auditoria e logs

--- server/app.py
+++ server/app.py
@@ -1,7 +1,14 @@
+import logging
 from flask import Flask, request, render_template, redirect
-
 import sqlite3
 import bcrypt
+
+# Configuração do logger
+logging.basicConfig(
+    filename='app.log',  # Nome do arquivo de log
+    level=logging.INFO,  # Nível de log (INFO, WARNING, ERROR, etc.)
+    format='%(asctime)s - %(levelname)s - %(message)s'  # Formato do log
+)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '181369b4a106a6478ebdb64ba2c5e009'
@@ -24,9 +31,11 @@ def login():
 
         if usuario and bcrypt.checkpw(senha.encode('utf-8'), usuario[0]):
             mensagem = f"Bem-vindo(a), {nome}!"
+            logging.info(f"Login bem-sucedido para o usuário: {nome}")
             return render_template('login.html', mensagem=mensagem, sucesso=True)
         else:
             mensagem = "Credenciais inválidas. Tente novamente."
+            logging.warning(f"Tentativa de login falhou para o usuário: {nome}")
             return render_template('login.html', mensagem=mensagem, sucesso=False)
     return render_template('login.html')
 
@@ -39,8 +48,10 @@ def consulta():
     try:
         c.execute(query)
         resultados = c.fetchall()
+        logging.info(f"Consulta realizada com o termo: {termo}")
     except Exception as e:
         resultados = [("Erro", str(e), "")]
+        logging.error(f"Erro na consulta com o termo: {termo} - {e}")
     conn.close()
     return render_template('index.html', resultados=resultados)
 
@@ -56,14 +67,20 @@ def inserir():
 
     conn = sqlite3.connect('banco.db')
     c = conn.cursor()
-    c.execute("INSERT INTO pessoas (nome, profissao, salario, senha) VALUES (?, ?, ?, ?)", (nome, profissao, salario, hashed_senha))
-    conn.commit()
-    conn.close()
+    try:
+        c.execute("INSERT INTO pessoas (nome, profissao, salario, senha) VALUES (?, ?, ?, ?)", (nome, profissao, salario, hashed_senha))
+        conn.commit()
+        logging.info(f"Novo usuário inserido: {nome}, profissão: {profissao}, salário: {salario}")
+    except Exception as e:
+        logging.error(f"Erro ao inserir usuário: {nome} - {e}")
+    finally:
+        conn.close()
 
     return redirect('/')
 
 @app.route('/logout')
 def logout():
+    logging.info("Usuário deslogado.")
     return redirect('/login')  # redirecionamento
 
 if __name__ == '__main__':
